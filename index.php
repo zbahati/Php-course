65073ca90c41686c1e45bca573bd754f5c541414
zbahati <bahatizirimwabagabo@gmail.com>
2024-01-06 16:16:40 +0200

    Add json file

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
 <?php
-// strtotime function
 
-// var_dump(strtotime(datetime: "7 months ago"));
+// $people = '
+// {
+// "name": {
+//     "age": 20,
+//     "address" : "gisenyi"
+// }
+// }';
 
-// echo strtotime("today")
-// echo  date(format: "y-m-d")
-// echo date(format: 'H:i:s Y-M jS T');
+// $json = json_decode($people,associative: true);
 
-// $date = '2024-1-03';
-// $today = date('y-m-d');
+// print_r($json);
 
-// $diff = (strtotime($today) - strtotime($date));
 
-// echo $diff/(60*60*24);
+$arr = [
+   [ "name"=> "Bahati",
+    "age"=>20,
+    "address"=> "gisenyi"],
+   [ "name"=> "James",
+    "age"=>20,
+    "address"=> "Rubavu"],
+   [ "name"=> "Gisa",
+    "age"=>20,
+    "address"=> "Nyanza"],
+];
 
-// scenario :
-// we are requesting the application that we can use in our borrowed company
-// we send money
-
-$date = date("Y-m-d", strtotime("5 days ago"));
-$today = date("Y-m-d");
-$amount = 0;
-$diff = (strtotime($today) - strtotime($date)) / (60 * 60 * 24);
-if ($diff > 2) {
-    $startcharg = $diff - 2;
-    $amount += 100 * $startcharg;
-}
-
-echo "you charge now is " . $amount . "frw";
+$json = json_encode($arr);
+echo $json;
