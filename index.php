163995ba6d53bf82f587ab085a321f4c61a4e848
zbahati <bahatizirimwabagabo@gmail.com>
2024-01-06 12:24:17 +0200

    Variable introduction

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-echo "Hello World!";
-echo "Hello class";
-echo "Hello student";
-echo "This is best start";
-echo "This is our first Project";
+
+// How to declare a variable
+// $variable = 10;
+// $var_a = 20;
+// $name = "Bahati";
+
+// echo $name;
+
+// how is the best way of creating variable
+
+// $variable = 10;
+// $_variable = 20;
+// $varible111 = 'Testing';
+
+// Invalid creation of Variables
+// $1variable = 10;
+
+?>
