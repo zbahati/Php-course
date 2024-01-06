1217a52a6dc2a54cca8be96f4915c7dc6eac4793
zbahati <bahatizirimwabagabo@gmail.com>
2024-01-06 12:50:52 +0200

    Add datatype

--- index.php
+++ index.php
@@ -1,19 +1,53 @@
 <?php
+// PHP has 8 dataTypes
 
-// How to declare a variable
-// $variable = 10;
-// $var_a = 20;
+// Strings
+
 // $name = "Bahati";
+// $address = 'gisenyi';
 
-// echo $name;
+// make sure you understand difference btn single and double quotes
 
-// how is the best way of creating variable
+// echo $sentence = "$name is living in $address";
 
-// $variable = 10;
-// $_variable = 20;
-// $varible111 = 'Testing';
+// Interger
 
-// Invalid creation of Variables
-// $1variable = 10;
+use function PHPSTORM_META\type;
 
-?>
+// $number_student = 10;
+// echo $number_student;
+
+// Double or Float
+//double hase 64 bit in size
+// float has 32 bit in size that is only the diffrence
+
+// $amout = 100.2;
+// var_dump($amout);
+// // Boolean
+// var_dump(value: 2 === '2');
+
+// Array
+
+// $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// print_r($arr);
+
+// $arr2 = [
+//   'name'=> 'Bahati',
+//   'age'=> 10,
+// ];
+
+// print_r($arr2);
+
+// echo $arr2['name'];
+
+// Object
+
+// $date = new DateTime();
+
+// null
+
+// $a = nULL;
+// echo $a;
+// var_dump($a);
+
+// resources
